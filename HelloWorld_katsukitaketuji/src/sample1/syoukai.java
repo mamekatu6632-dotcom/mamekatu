e47833e4642d921691ba62e61cf17bf1157dcfb1
katuk <katuk@katsuki>
2025-12-16 21:06:33 +0900

    自己紹介

---
+++ HelloWorld_katsukitaketuji/src/sample1/syoukai.java
@@ -0,0 +1,18 @@
+package sample1;
+
+public class syoukai {
+
+	public static void main(String[] args) {
+		String name ="竹辻　勝生";
+		int age = 30;
+		String hobby ="映画 鑑賞";
+		
+		System.out.println("私の名前は"+ name +"です");
+         
+		System.out.println("年齢は"+ age +"歳です");
+		
+		System.out.println("趣味は" + hobby + "です");
+		
+	}
+
+}
