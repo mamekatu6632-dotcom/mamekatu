d3b4574638bda22c9db0049337dc47f2e8d322bf
katuk <katuk@katsuki>
2025-12-17 01:01:49 +0900

    java基礎問題_1   Q1

---
+++ HelloWorld_katsukitaketuji/src/curriculum/a/Question1.java
@@ -0,0 +1,28 @@
+package curriculum.a;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		byte byteNum = 0; //byte型の変数　byteNum を宣言　初期値0
+		System.out.println(byteNum);
+		short shortNum = 0;//short型の変数　shortNumを宣言　初期値0
+		System.out.println(shortNum);
+		int intNum = 0;//int型の変数　intNumを宣言　初期値0
+		System.out.println(intNum);
+		long longNum = 0L;//long型の変数 longNumを宣言　初期値0
+		System.out.println(longNum);
+		float floatNum = 0.0f;//float型の変数 floatNumを宣言　初期値0.0f
+		System.out.println(floatNum);
+		double doubleNum = 0.0;//double型の変数　doubleNumを宣言 初期値0.0
+		System.out.println(doubleNum);
+		char letter = 'a';//char型の変数　letterを宣言　初期値"a"
+		System.out.println(letter);
+		String letters = "";//String型の変数　lettersを宣言　初期値""
+		System.out.println(letters);
+		boolean isBoolean = false;//boolean型の変数　isBooleanを宣言　初期値false
+		System.out.println(isBoolean);
+		
+
+	}
+
+}
