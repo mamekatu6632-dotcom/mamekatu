3a90f383a864a07c1930648d6bb74db4da0a9666
katuk <katuk@katsuki>
2025-12-17 01:20:08 +0900

    java基礎問題　Q2

---
+++ HelloWorld_katsukitaketuji/src/curriculum/a/Question2.java
@@ -0,0 +1,46 @@
+package curriculum.a;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		byte byteNum = 0; //byte型の変数　byteNum を宣言　初期値0
+		System.out.println(byteNum);
+		short shortNum = 0;//short型の変数　shortNumを宣言　初期値0
+		System.out.println(shortNum);
+		int intNum = 0;//int型の変数　intNumを宣言　初期値0
+		System.out.println(intNum);
+		long longNum = 0L;//long型の変数 longNumを宣言　初期値0
+		System.out.println(longNum);
+		float floatNum = 0.0f;//float型の変数 floatNumを宣言　初期値0.0f
+		System.out.println(floatNum);
+		double doubleNum = 0.0;//double型の変数　doubleNumを宣言 初期値0.0
+		System.out.println(doubleNum);
+		char letter = 'a';//char型の変数　letterを宣言　初期値"a"
+		System.out.println(letter);
+		String letters = "";//String型の変数　lettersを宣言　初期値""
+		System.out.println(letters);
+		boolean isBoolean = false;//boolean型の変数　isBooleanを宣言　初期値false
+		System.out.println(isBoolean);
+		
+		
+		byteNum = 10; //数byteNumに10を代入
+		System.out.println(byteNum);
+		shortNum =100;//変数shortNumに100を代入
+		System.out.println(shortNum);
+		intNum = 1000;//変数intNumに1000を代入
+		System.out.println(intNum);
+		longNum =10000;//変数longNumに10000を代入
+		System.out.println(longNum);
+		floatNum =9.5f;//変数floatNumに9.5fを代入
+		System.out.println(floatNum);
+		doubleNum =10.5;//変数doubleNumに10.5を代入
+		System.out.println(doubleNum);
+		letter ='a';//変数letterに"a"を代入
+		System.out.println(letter);
+		letters = "ハロー";//変数lettersに"ハロー"を代入
+		System.out.println(letters);
+		isBoolean = true;//変数isBooleanにtrueを代入
+		System.out.println(isBoolean);
+	}
+
+}
