40a1e1bbc3d9e62183f9305d629bb421fea31a02
katuk <katuk@katsuki>
2025-12-31 19:21:18 +0900

    Question3終了しました

--- HelloWorld_katsukitaketuji/src/curriculum/b/Question2.java
+++ HelloWorld_katsukitaketuji/src/curriculum/b/Question2.java
@@ -46,125 +46,135 @@ public class Question2 {
 
 		System.out.println("いずれかの値を代入");
 
-		int num = sc.nextInt();
-		{
+		String input5 = sc.nextLine();
 
-			if (num > 0) {
-				System.out.println("正の数です");
-			} else if (num == 0) {
-				System.out.println("0です");
-			} else if (num < 0) {
-				System.out.println("負の数です");
-			}
-
-		}
-
-		System.out.println("いずれかの値を代入");
-
-		int value = sc.nextInt();
-		{
-
-			if (value % 2 == 0) {
-				System.out.println("偶数です");
-			} else
+		if (!input5.trim().isEmpty()) {
+			int num = Integer.parseInt(input5);
 
 			{
-				System.out.println("奇数です");
+
+				if (num > 0) {
+					System.out.println("正の数です");
+				} else if (num == 0) {
+					System.out.println("0です");
+				} else if (num < 0) {
+					System.out.println("負の数です");
+				}
+
 			}
 
+			System.out.println("いずれかの値を代入");
+
+			String input6 = sc.nextLine();
+
+			if (!input6.trim().isEmpty()) {
+				int value = Integer.parseInt(input6);
+
+				{
+
+					if (value % 2 == 0) {
+						System.out.println("偶数です");
+					} else
+
+					{
+						System.out.println("奇数です");
+					}
+
+				}
+				System.out.println("0から100のいずれかの値を代入");
+				String input = sc.nextLine();
+
+				if (!input.trim().isEmpty()) {
+					int Score = Integer.parseInt(input);
+
+					if (Score >= 90) {
+						System.out.println("優");
+					} else if (Score >= 70) {
+						System.out.println("良");
+					} else if (Score >= 50) {
+						System.out.println("可");
+					} else {
+						System.out.println("不可");
+					}
+
+					System.out.println("文字を入力してください");
+					String input1 = sc.nextLine();
+
+					if (input1 == null || input1.isEmpty()) {
+						System.out.println("入力が無効です");
+					} else {
+						System.out.println(input1);
+					}
+
+					String input2 = sc.nextLine();
+
+					int day;
+
+					day = Integer.parseInt(input2);
+
+					switch (day) {
+					case 1:
+						System.out.println("今日は月曜日です");
+						break;
+					case 2:
+						System.out.println("今日は火曜日です");
+						break;
+					case 3:
+						System.out.println("今日は水曜日です");
+						break;
+					case 4:
+						System.out.println("今日は木曜日です");
+						break;
+					case 5:
+						System.out.println("今日は金曜日です");
+						break;
+					case 6:
+						System.out.println("今日は土曜日です");
+						break;
+					case 7:
+						System.out.println("今日は日曜日です");
+						break;
+					default:
+						System.out.println("無効な入力です");
+
+					}
+
+					String input3 = sc.nextLine();
+
+					int month;
+
+					month = Integer.parseInt(input3);
+
+					switch (month) {
+					case 1:
+					case 2:
+					case 12:
+						System.out.println("冬");
+						break;
+					case 3:
+					case 4:
+					case 5:
+						System.out.println("春");
+						break;
+					case 6:
+					case 7:
+					case 8:
+						System.out.println("夏");
+						break;
+					case 9:
+					case 10:
+					case 11:
+						System.out.println("秋");
+						break;
+					default:
+						System.out.println("無効な月です");
+					}
+
+					sc.close();
+
+				}
+
+			}
 		}
-		System.out.println("0から100のいずれかの値を代入");
-		String input = sc.nextLine();
-
-		if (!input.trim().isEmpty()) {
-			int Score = Integer.parseInt(input);
-
-			if (Score >= 90) {
-				System.out.println("優");
-			} else if (Score >= 70) {
-				System.out.println("良");
-			} else if (Score >= 50) {
-				System.out.println("可");
-			} else {
-				System.out.println("不可");
-			}
-
-			System.out.println("文字を入力してください");
-			String input1 = sc.nextLine();
-
-			if (input1 == null || input1.isEmpty()) {
-				System.out.println("入力が無効です");
-			} else {
-				System.out.println(input1);
-			}
-
-			String input2 = sc.nextLine();
-
-			int day;
-
-			day = Integer.parseInt(input2);
-
-			switch (day) {
-			case 1:
-				System.out.println("今日は月曜日です");
-				break;
-			case 2:
-				System.out.println("今日は火曜日です");
-				break;
-			case 3:
-				System.out.println("今日は水曜日です");
-				break;
-			case 4:
-				System.out.println("今日は木曜日です");
-				break;
-			case 5:
-				System.out.println("今日は金曜日です");
-				break;
-			case 6:
-				System.out.println("今日は土曜日です");
-				break;
-			case 7:
-				System.out.println("今日は日曜日です");
-				break;
-			default:
-				System.out.println("無効な入力です");
-
-			}
-
-			String input3 = sc.nextLine();
-
-			int month;
-
-			month = Integer.parseInt(input3);
-
-			switch (month) {
-			case 1:
-			case 2:
-			case 12:
-				System.out.println("冬");
-				break;
-			case 3:
-			case 4:
-			case 5:
-				System.out.println("春");
-				break;
-			case 6:
-			case 7:
-			case 8:
-				System.out.println("夏");
-				break;
-			case 9:
-			case 10:
-			case 11:
-				System.out.println("秋");
-				break;
-			default:
-				System.out.println("無効な月です");
-			}
-
-			sc.close();
-
-		}
-
 	}
 }
--- HelloWorld_katsukitaketuji/src/curriculum/b/Question3.java
+++ HelloWorld_katsukitaketuji/src/curriculum/b/Question3.java
@@ -1,5 +1,10 @@
 package curriculum.b;
 
+import java.util.Random;
+import java.util.Scanner;
+
+
+
 public class Question3 {
 
 	public static void main(String[] args) {
@@ -26,6 +31,96 @@ public class Question3 {
 			}
 			System.out.println();
 		}
+
+		int count = 1;
+		while (count <= 10) {
+			System.out.println(count);
+			count++;
+		}
+		int count2 = 2;
+		while (count2 <= 20) {
+			System.out.println(count2);
+			count2 += 2;
+		}
+
+		int count3 = 10;
+		while (count3 >= 1) {
+			System.out.println(count3);
+			count3--;
+		}
+		int count4 = 1;
+		int num = 0;
+		while (count4 <= 100) {
+			num += count4;
+			count4++;
+		}
+		System.out.println(num);
+
+		Scanner sc = new Scanner(System.in);
+		
+	
+		String input;
+		while (true) {
+			System.out.print("数値を入力");
+			input = sc.nextLine();
+			int sum1 = Integer.parseInt(input);
+			if (sum1 == 0) {
+				System.out.println("終了しました");
+				break;
+			}
+
+		}
+		for (int a = 1; a <= 9; a++) {
+			for (int b = 1; b <= 9; b++) {
+				System.out.printf("%02d * %02d = %02d ||", a, b, a * b);
+
+			}
+			System.out.println();
+
+		}
+		
+		Random rand = new Random();
+		
+		System.out.print("商品名を入力");
+		String input2 = sc.nextLine();
+		String[] items = input2.split("、");
+		
+		
+
+		for (String item : items) {
+			int tv = rand.nextInt(12);
+			int display = 11 - tv;
+
+			switch (item) {
+
+			case "テレビ":
+			case "ディスプレイ":
+				System.out.println(
+						item + "残りの台数は" + (item.equals("テレビ") ? tv : display) + "台です");
+				break;
+
+			case "パソコン":
+				System.out.println("パソコンの残り台数は" + rand.nextInt(12) + "台です");
+				break;
+
+			case "冷蔵庫":
+				System.out.println("冷蔵庫の残り台数は" + rand.nextInt(12) + "台です");
+				break;
+			case "扇風機":
+				System.out.println("扇風機の残り台数は" + rand.nextInt(12) + "台です");
+				break;
+			case "洗濯機":
+				System.out.println("洗濯機の残り台数は" + rand.nextInt(12) + "台です");
+				break;
+			case "加湿器":
+				System.out.println("加湿器の残り台数は" + rand.nextInt(12) + "台です");
+				break;
+			default:
+				System.out.println("『" + item + "』は指定の商品ではありません");
+
+			}
+		}
+		sc.close();
 
 	}
 }
