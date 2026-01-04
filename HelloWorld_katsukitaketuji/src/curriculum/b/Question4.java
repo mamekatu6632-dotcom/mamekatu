af7db7bf9b4cf85c231bbda9a56ffeeb3c7818c4
katuk <katuk@katsuki>
2026-01-04 20:31:46 +0900

    Q10まで

--- HelloWorld_katsukitaketuji/src/curriculum/b/Question4.java
+++ HelloWorld_katsukitaketuji/src/curriculum/b/Question4.java
@@ -1,7 +1,5 @@
 package curriculum.b;
 
-import java.util.Scanner;
-
 public class Question4 {
 
 	public static void main(String[] args) {
@@ -48,31 +46,76 @@ public class Question4 {
 			
 		}*/
 
-		Scanner sc = new Scanner(System.in);
-
+		/*Scanner sc = new Scanner(System.in);
+		
 		int[] number = { 4, 7, 10, 15, 20 };
 		String input;
 		while (true) {
 			System.out.print("数字を入力");
 			input = sc.nextLine();
 			int sum = Integer.parseInt(input);
-
+		
 			boolean found = false;
 			for (int n : number) {
 				if (sum == n) {
 					found = true;
 					break;
-
+		
 				}
 			}
-
+		
 			if (found) {
 				System.out.println(+sum + "は配列に含まれています");
 			} else {
 				System.out.println(+sum + "は配列に含まれていません");
 			}
+		
+		}*/
+
+		/*int[][] array = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+		for(int i = 0;i < array.length;i++) {
+			for(int j =0; j<array[i].length;j++) {
+				System.out.println(array[i][j]+"");
+			}
+			System.out.println();
+		}*/
+
+		/*int[][] array = { { 10, 20, 30, }, { 40, 50, 60 }, { 70, 80, 90 } };
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int p = 0; p < array[i].length; p++) {
+				sum += array[i][p];
+			}
+		}
+		System.out.println(sum);*/
+
+		/*int[][] array = { { 12, 15, 8 }, { 6, 19, 25 }, { 30, 2, 10 } };
+		int max = array[0][0];
+		int min = array[0][0];
+		for (int[] row : array) {
+			for (int n : row) {
+				if (n > max) {
+					max = n;
+				}
+				if (n < min) {
+
+					min = n;
+				}
+
+			}
 
 		}
+		System.out.println("最大"+max);
+		System.out.println("最小"+min);*/
+		
+		int[][][]array = {{{1,2},{3,4}},{{5,6,},{7,8}}};
+		for(int[][] plane : array) {
+			for(int[] row : plane) {
+				for(int n : row) {
+					System.out.println(n);
+				}
+			}
+		}
+		
 	}
-
 }
